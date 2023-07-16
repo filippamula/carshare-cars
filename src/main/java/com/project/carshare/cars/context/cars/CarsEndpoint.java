7b58f7835198b7f17ad841de0a28a963887a79ee
filip <filippamula@onet.pl>
2023-07-16 12:33:11 +0200

    added descriptions in swagger

--- src/main/java/com/project/carshare/cars/context/cars/CarsEndpoint.java
+++ src/main/java/com/project/carshare/cars/context/cars/CarsEndpoint.java
@@ -2,6 +2,7 @@ package com.project.carshare.cars.context.cars;
 
 import com.project.carshare.cars.context.cars.dto.CarRequestDto;
 import com.project.carshare.cars.context.cars.dto.CarInfoResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,23 +18,27 @@ public class CarsEndpoint {
 
     private final CarsService carsService;
 
+    @Operation(description = "Returns list of cars in offer")
     @GetMapping()
     public ResponseEntity<List<CarInfoResponseDto>> getAvailableCars(){
         return ResponseEntity.ok(carsService.getAvailableCars());
     }
 
     //ADMIN
+    @Operation(description = "Returns list of all cars, requires admin role")
     @GetMapping("/all")
     public ResponseEntity<List<CarInfoResponseDto>> getAllCars() {
         return ResponseEntity.ok(carsService.getAllCars());
     }
 
+    @Operation(description = "Adds a car requires admin role")
     @PostMapping("/add")
     public ResponseEntity<Void> addCar(@RequestBody CarRequestDto request) {
         carsService.addCar(request);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(description = "Modifies car, requires role admin")
     @PatchMapping("/{cardId}")
     public ResponseEntity<Void> modifyCar(
             @PathVariable UUID cardId,
@@ -43,6 +48,7 @@ public class CarsEndpoint {
         return ResponseEntity.ok().build();
     }
 
+    @Operation(description = "Adds an image to a car, requires role admin")
     @PostMapping("/{carId}/add-image")
     public ResponseEntity<Void> addCarImage(
             @PathVariable UUID carId,
@@ -52,6 +58,7 @@ public class CarsEndpoint {
         return ResponseEntity.ok().build();
     }
 
+    @Operation(description = "Changes car availability, requires role admin")
     @PostMapping("/{carId}/change-car-availability")
     public ResponseEntity<Void> addCarToFleet(@PathVariable UUID carId){
         carsService.changeCarAvailability(carId);
